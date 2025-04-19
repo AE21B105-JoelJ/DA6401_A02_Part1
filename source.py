5066b4e04b3d5e59875fa32dc52287c0125a3983
AE21B105-JoelJ <ae21b105@smail.iitm.ac.in>
2025-04-19 19:20:31 +0530

    contains the util functions

--- source.py
+++ source.py
@@ -11,7 +11,7 @@ from lightning.pytorch import Trainer
 from torchvision import transforms
 from torch.utils.data import DataLoader, Subset
 from lightning.pytorch.loggers import WandbLogger
-from torchmetrics import Precision
+from torchmetrics import Accuracy
 from torchvision import datasets, transforms
 from sklearn.model_selection import train_test_split
 import os
@@ -44,7 +44,7 @@ class CNN_(nn.Module):
         
         # Some assertions to be made #
         assert config["no_of_conv_blocks"]==len(config["no_of_filters"]), "The filter number do not match with number of conv layers"
-        assert config["no_of_conv_blocks"]==len(config["filters_sizes"]), "The filter sizes do not match with number of conv layers"
+        assert config["no_of_conv_blocks"]==len(config["filter_sizes"]), "The filter sizes do not match with number of conv layers"
         assert config["no_of_conv_blocks"]==len(config["conv_strides"]), "The strides do not match with number of conv layers"
         assert config["no_of_conv_blocks"]==len(config["conv_padding"]), "The padding do not match with number of conv layers"
         assert config["no_of_conv_blocks"]==len(config["max_pooling_stride"]), "The max pooling stride do not match with number of conv layers"
@@ -59,12 +59,12 @@ class CNN_(nn.Module):
                 in_channels = config["no_of_filters"][block_no-1]
             out_channels = config["no_of_filters"][block_no]
             filter_size = config["filter_sizes"][block_no]
-            stride = config["conv_stride"][block_no]
-            padding = config["conv_padding"]
+            stride = config["conv_strides"][block_no]
+            padding = config["conv_padding"][block_no]
             if padding == None:
-                padding = (filter_size - 1)/2 if filter_size > 1 else 0
+                padding = int((filter_size - 1)/2) if filter_size > 1 else 0
             # Defining the block to add to conv_blocks #
-            block_add = nn.sequential(
+            block_add = nn.Sequential(
                 nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=filter_size, stride=stride, padding=padding),
                 nn.BatchNorm2d(num_features=out_channels) if config["batch_norm_conv"] else nn.Identity(),
                 return_activation_function(activation=config["conv_activation"][block_no]),
@@ -78,7 +78,7 @@ class CNN_(nn.Module):
         self.conv_blocks = nn.Sequential(*conv_blocks)
 
         # Calculating the size of the output #
-        dummy_in = torch.randn(size=(1, config["input_channels"],config["input_size"], config["input_size"]))
+        dummy_in = torch.randn(size=(1, config["input_channels"],config["input_size"][0], config["input_size"][1]))
         dummy_out = self.conv_blocks(dummy_in).flatten()
         flat_size = len(dummy_out)
 
@@ -129,7 +129,7 @@ class Lightning_CNN(L.LightningModule):
         self.optimizer = torch.optim.Adam(self.parameters(), lr = config["learning_rate"])
 
         # Defining the metrics
-        self.prec_metric = Precision(task="multiclass", num_classes=config["num_classes"], average="weighted")
+        self.prec_metric = Accuracy(task="multiclass", num_classes=config["num_classes"], average="weighted")
 
     def forward(self, x):
         return self.model(x)
@@ -174,7 +174,7 @@ class Lightning_CNN(L.LightningModule):
 
 # class to orient (all images to landscape)
 class OrientReshape:
-    def __init__(self, size = (256, 256)):
+    def __init__(self, size = (224, 224)):
         self.size = size
     
     def __call__(self, img):
@@ -188,13 +188,14 @@ class OrientReshape:
     
 
 # Data augementation and transforms
-def create_data_augment_compose(input_size = (256, 256)):
+def create_data_augment_compose(input_size = (224, 224)):
     data_transforms = {
         "orient_" : transforms.Compose([
             OrientReshape(size=input_size),
-            transforms.ToTensor()
+            transforms.ToTensor(),
         ]),
         "train_" : transforms.Compose([
+            OrientReshape(size=input_size),
             transforms.RandomHorizontalFlip(p = 0.3),
             transforms.RandomVerticalFlip(p = 0.3),
             transforms.RandomRotation(degrees=15),
@@ -202,13 +203,13 @@ def create_data_augment_compose(input_size = (256, 256)):
             transforms.GaussianBlur(kernel_size=3),
             transforms.ToTensor(),
             transforms.RandomErasing(p = 0.2, scale=(0.02, 0.075)),
-        ])
+        ]),
     }
 
     return data_transforms
 
 # Create a dataset with the image folders
-def create_dataset_image_folder(path_, input_size = (256,256)):
+def create_dataset_image_folder(path_, input_size = (224,224)):
     # Getting the transform
     data_transforms = create_data_augment_compose(input_size)
     # Path to dataset
@@ -231,10 +232,6 @@ def create_dataloaders(batch_size, num_workers, train_dataset, val_dataset, is_d
     # Transforming the dataset with transforms
     if is_data_aug:
         train_dataset.dataset.transform = data_transforms['train_']
-    else:
-        train_dataset.dataset.transform = data_transforms['orient_']
-
-    val_dataset.dataset.transform = data_transforms['orient_']
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,drop_last=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
@@ -245,9 +242,6 @@ def get_test_dataloader(path_, data_transforms):
     # Path to dataset #
     data_dir = path_ #os.path.join(os.path.abspath(""), "nature_12K/inaturalist_12K/val/")
     test_dataset = datasets.ImageFolder(root=data_dir, transform=data_transforms["orient_"])
-
-    # Applying transforms to datasets #
-    test_dataset.dataset.transform = data_transforms["orient_"] 
 
     batch_size = 20
     num_workers = 4 # Adaptive number of workers
